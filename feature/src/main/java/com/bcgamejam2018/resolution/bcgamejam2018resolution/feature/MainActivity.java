4e01e4fece3bd4da83ef5038ec5571b7d8e5fea2
Eric Kim <bosung90@gmail.com>
2018-02-09 22:20:34 -0800

    initial commit

---
+++ feature/src/main/java/com/bcgamejam2018/resolution/bcgamejam2018resolution/feature/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bcgamejam2018.resolution.bcgamejam2018resolution.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
