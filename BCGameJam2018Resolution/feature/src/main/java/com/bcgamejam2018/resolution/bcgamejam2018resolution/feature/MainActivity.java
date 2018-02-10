bbff5b302b6a1e6fbea68202ca0dee7c13e5feff
Eric Kim <bosung90@gmail.com>
2018-02-09 23:35:12 -0800

    Added Image Button, show toast when button is pressed. Change orientation of the app to landscape

--- BCGameJam2018Resolution/feature/src/main/java/com/bcgamejam2018/resolution/bcgamejam2018resolution/feature/MainActivity.java
+++ BCGameJam2018Resolution/feature/src/main/java/com/bcgamejam2018/resolution/bcgamejam2018resolution/feature/MainActivity.java
@@ -2,6 +2,10 @@ package com.bcgamejam2018.resolution.bcgamejam2018resolution.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton startImageBtn = findViewById(R.id.gameStartButton);
+
+        startImageBtn.setOnClickListener(new ImageButton.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "Game Start Button Pressed!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
